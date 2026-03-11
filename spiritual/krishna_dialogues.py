2d3693d86ed7a22935f2db8f16c5dbdf1378ea5b
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-03-11 13:42:55 +0530

    Create krishna_dialogues.py

---
+++ spiritual/krishna_dialogues.py
@@ -0,0 +1,15 @@
+import random
+
+KRISHNA_LINES = [
+
+"कर्म करो फल की चिंता मत करो",
+
+"धर्म की रक्षा के लिए युद्ध आवश्यक है",
+
+"अधर्म का अंत निश्चित है"
+]
+
+
+def krishna_message():
+
+    return random.choice(KRISHNA_LINES)
