512c5d241aa6967ef60a7585022f502c34aa70a6
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-03-11 13:36:22 +0530

    Create legendary_astras.py

---
+++ weapons/legendary_astras.py
@@ -0,0 +1,17 @@
+LEGENDARY_ASTRAS = {
+
+    "sudarshan_chakra": {
+        "damage": 1000,
+        "owner": "krishna"
+    },
+
+    "gandiva": {
+        "damage": 650,
+        "owner": "arjuna"
+    },
+
+    "bhima_gada": {
+        "damage": 600,
+        "owner": "bhima"
+    }
+}
