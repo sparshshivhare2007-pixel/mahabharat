898bde8a913037271070810ae20d3663460954cb
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-03-11 13:39:39 +0530

    Create shop.py

---
+++ economy/shop.py
@@ -0,0 +1,17 @@
+SHOP_ITEMS = {
+
+    "gada": {
+        "price": 200,
+        "damage": 50
+    },
+
+    "bow": {
+        "price": 300,
+        "damage": 70
+    },
+
+    "armor": {
+        "price": 400,
+        "defense": 60
+    }
+}
