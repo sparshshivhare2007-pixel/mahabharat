bfdde9808ef92932398072d90e41973ac11f1dca
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-03-11 13:35:47 +0530

    Create astras.py

---
+++ weapons/astras.py
@@ -0,0 +1,22 @@
+ASTRAS = {
+    "brahmastra": {
+        "damage": 900,
+        "rarity": "mythic",
+        "desc": "ब्रह्मा का दिव्य अस्त्र"
+    },
+    "pashupatastra": {
+        "damage": 850,
+        "rarity": "legendary",
+        "desc": "भगवान शिव का अस्त्र"
+    },
+    "narayanastra": {
+        "damage": 700,
+        "rarity": "legendary",
+        "desc": "नारायण का प्रचंड अस्त्र"
+    },
+    "agneyastra": {
+        "damage": 450,
+        "rarity": "rare",
+        "desc": "अग्नि देव का अस्त्र"
+    }
+}
