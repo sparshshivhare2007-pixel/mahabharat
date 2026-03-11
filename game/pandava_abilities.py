3a294cfbe710ad1ea8bb16bb9ed8c23f04b30165
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-03-11 13:15:25 +0530

    Create pandava_abilities.py

---
+++ game/pandava_abilities.py
@@ -0,0 +1,18 @@
+abilities = {
+
+"arjuna":{
+"skill":"divine arrow",
+"damage":300
+},
+
+"bhima":{
+"skill":"gada smash",
+"damage":350
+},
+
+"yudhishthira":{
+"skill":"dharma strike",
+"damage":200
+}
+
+}
